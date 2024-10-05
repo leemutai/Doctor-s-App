cc90f76e2da0107eb64ffbee6b180efbd070f6cf
leemutai <mutailee361@gmail.com>
2024-10-05 16:26:53 +0300

    routed the assests folder,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div>
-      
+      <img src={assets.logo} alt="" />
+      <ul>
+        <NavLink>
+            <li>HOME</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>ALL DOCTORS</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>ABOUT</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>CONTACT</li>
+            <hr />
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create account</button>
+      </div>
     </div>
   )
 }
