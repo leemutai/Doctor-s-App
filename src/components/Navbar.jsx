1708f42d5a5a53377db31aa241fb7aa9fa7428ab
leemutai <mutailee361@gmail.com>
2024-10-06 00:26:30 +0300

    initialized state variables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar= () => {
 
   const navigate = useNavigate()
+
+  const [showMenu, setShowMenu] = useState(false)
+  const [token, setToken] = useState(true)
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
