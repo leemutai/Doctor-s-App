ed10905e6ac705b9756724d5be5934ff3b683a87
leemutai <mutailee361@gmail.com>
2024-10-06 12:42:04 +0300

    modified tailwindconfig, added grid template feature

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -9,10 +9,10 @@ function TopDoctors() {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0,10).map((item,index)=>(
             <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
-                <img src={item.image} alt="" />
-                <div>
-                    <div>
-                        <p></p><p>Available</p>
+                <img className='bg-blue-50' src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
                     </div>
                     <p>{item.name}</p>
                     <p>{item.speciality}</p>
