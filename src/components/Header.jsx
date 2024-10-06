42943bd15e0b6085b4c9e11502ffc2f41289a021
leemutai <mutailee361@gmail.com>
2024-10-06 10:48:31 +0300

    added a new file, specialitymenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ function Header() {
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
               Book Appoinment <br/> with Trusted Doctors  
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors,<br/> select a doctor of your choice and
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-28' src={assets.group_profiles} alt="" />
+                <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> select a doctor of your choice and
                 schedule your appointment hassle-free.</p>
             </div>
-            <a href="">
-                Boook appointment <img src={assets.arrow_icon} alt="" />
+            <a href="" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+                Boook appointment <img className='w-3' src={assets.arrow_icon} alt="" />
             </a>
 
         </div>
