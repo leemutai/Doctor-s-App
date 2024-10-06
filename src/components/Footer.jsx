5072b720b26af77fbde60b158da11eeec2f1be90
leemutai <mutailee361@gmail.com>
2024-10-06 16:37:13 +0300

    modified footer page, home page completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets'
 
 function Footer() {
   return (
-    <div>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/* ------Left Section------- */}
         <div>
-            <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <img className='mb-5 w-40' src={assets.logo} alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
         </div>
         {/* ------Middle/center Section------- */}
         <div>
-            <p>COMPANY</p>
-            <ul>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>Home</li>
                 <li>About us</li>
                 <li>Contact us</li>
@@ -23,8 +23,8 @@ function Footer() {
         </div>
         {/* ------Right Section------- */}
         <div>
-            <p>GET IN TOUCH</p>
-            <ul>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>+254797001959</li>
                 <li>mutailee361@gmail.com</li>
             </ul>
@@ -33,7 +33,7 @@ function Footer() {
       {/* ------Copyright Text Section------- */}
       <div>
         <hr />
-        <p>Copyright © 2024 LeeSpien - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright © 2024 LeeSpien - All Right Reserved.</p>
       </div>
     </div>
   )
