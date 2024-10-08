f484d3c92f05f182ff2109c8c90eb98b0551ee47
leemutai <mutailee361@gmail.com>
2024-10-08 11:38:52 +0300

    Added clickable booking slots for date and time selection. Implemented UI highlighting for selected slots and added 'Book an appointment' button for finalization.

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -126,12 +126,10 @@ const Appointment = () => {
             {docSlots.length && docSlots[slotIndex].map((item,index)=> (
               <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
                 {item.time.toLowerCase()}
-
               </p>
-
             ))}
           </div>
-
+          <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
         </div>
       </div>
     )
